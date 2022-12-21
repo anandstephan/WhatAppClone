9a09cf5c82508df8c9296f88a6137d2ff39f0f11
anandstephan <anand9412868527@gmail.com>
2022-12-21 10:58:42 +0530

    feat: message screen with dummy chat message

--- screens/Message.jsx
+++ screens/Message.jsx
@@ -1,17 +1,43 @@
-import { View, Text,SafeAreaView,StyleSheet,StatusBar } from 'react-native'
+import { View, Text,SafeAreaView,StyleSheet,StatusBar,TextInput, ScrollView, FlatList } from 'react-native'
 import React from 'react'
+import { Entypo,Ionicons,AntDesign,FontAwesome,FontAwesome5 } from '@expo/vector-icons';
 import CustomHeader from '../components/customHeader'
+import { LeftChatMessage, RightChatMessage } from '../components/chatMessage';
+
 
 const Message = ({navigation}) => {
   return (
     <SafeAreaView style={styles.container}>
         <CustomHeader messageHeader={true} headerShown={true} navigation={navigation}/>
         <View style={styles.messageContainer}>
-          <View>
-    <Text>Hi</Text>
-          </View>
-          <View>
-      <Text>Message Chat</Text>
+        <FlatList 
+      showsVerticalScrollIndicator={false}
+        data={[1,2,3,4,5,6,7,8]}
+        renderItem={({item},idx) =><>
+            <LeftChatMessage/>
+    <RightChatMessage/>
+        </>}
+        />
+
+
+       
+ 
+          <View style={styles.bottomChatContainer}>
+            <View style={styles.textInputContainer}>
+            <Entypo name="emoji-happy" size={24} color="black" />
+            <View style={styles.leftGroup}>
+            <TextInput placeholder='Message' style={styles.textInput}/>
+            <View style={styles.leftGroupIcon}>
+            <AntDesign name="sharealt" size={24} color="black" />
+            <FontAwesome name="rupee" size={24} color="black" />
+            <FontAwesome5 name="camera" size={24} color="black" />
+            </View>
+            </View>
+        
+            </View>
+            <View style={styles.bottomIcon}>
+            <Ionicons name="ios-mic-sharp" size={30} color="white" />
+            </View>
           </View>
         </View>
     </SafeAreaView>
@@ -22,10 +48,49 @@ export default Message
 
 const styles = StyleSheet.create({
   container:{
-    marginTop:StatusBar.currentHeight
+    marginTop:StatusBar.currentHeight,
+    flex:1,
+
   },
   messageContainer:{
+
+    justifyContent:'space-between',
+    flex:1,
+    padding:5
+  },
+  textInputContainer:{
+    flexDirection:'row',
     borderWidth:2,
-    justifyContent:'space-evenly',
+    padding:5,
+    borderRadius:30,
+    flex:1
+  },
+  textInput:{
+    marginHorizontal:10,
+    flex:2
+  },
+  bottomChatContainer:{
+    flexDirection:"row",
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  bottomIcon:{
+    justifyContent:'center',
+    alignItems:"center",
+    backgroundColor:"black",
+    borderRadius:100
+  },
+  leftGroupIcon:{
+    flexDirection:'row',
+    justifyContent:"space-between",
+    alignItems:"center",
+
+    flex:1
+    
+  },
+  leftGroup:{
+    flexDirection:'row',
+    justifyContent:"space-between",
+    flex:1,
   }
 })
